eef043e8d152720ff6a247abc5e06515c239a2d1
Jonathan Vehns <jonathan@ChristophsMBP3.fritz.box>
2021-04-22 15:34:51 +0200

    Added changes

--- main.ino
+++ main.ino
@@ -7,7 +7,8 @@
 
 // Variables for automatic irrigation:
 int soilMoisture;
-const int moistureNorm = 0;  // The moisture we want to keep
+int nextMeasurement = 0;
+const int moistureNorm = 700;  // The moisture we want to keep
 const int moistureSensorPin = A0;
 const int pumpPin = 10;
 
@@ -30,8 +31,9 @@ void setup() {
   pinMode(moistureSensorPin, INPUT);
   // initialize pump
   pinMode(pumpPin, OUTPUT);
+  Serial.begin(9600);
   // initialize BME
-  bme.init();
+  //bme.init();
   
   setupLCD();
   setupSDCard();
@@ -43,13 +45,16 @@ void setup() {
 }
 
 void loop() {
-  checkMoisture();
-
+  if ( nextMeasurement % 5 == 0 ) checkMoisture();
+  
   openFile(bmeDataFileName);
   writeLine(getTimeFormated() + ";" + getPressureBar() + ";" + getPressurePascal() + ";" + getHumidity());
   closeFile();
   
   delay(1000);
+  if ( (nextMeasurement+3) % 5 == 0) stopPump(); 
+  // The pump should be only activated for a short time (3s)
+  nextMeasurement++;
 }
 
 //Automatic irrigation
@@ -63,8 +68,10 @@ void stopPump(){
 
 void checkMoisture(){
   soilMoisture = analogRead(moistureSensorPin);
-  if ( soilMoisture < moistureNorm ) startPump();
-  if ( soilMoisture > moistureNorm ) stopPump();
+  if ( soilMoisture > moistureNorm ) startPump();
+  /*if ( soilMoisture < moistureNorm ) stopPump();
+  Serial.println(String(map(soilMoisture,253,1019,100,0))+","+String(soilMoisture));
+  Serial.println("Grenzwert: "+String(map(moistureNorm,253,1019,100,0)));*/
 }
 
 //LCD
